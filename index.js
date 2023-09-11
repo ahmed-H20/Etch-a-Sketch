b55079f3a141b6cbf4b733007620df493f632bd8
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-09-11 04:33:28 +0300

    Make a grid constract

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < (16*16); i++) {
+for (let column = 0; column < 16; column++) {
+    for (let raw = 0; raw < 16; raw++) {
     const div = document.createElement('div');
     container.appendChild(div);
+    }
 }
