4755a12db45a7cc3acfb2d06dea910547247d5d7
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-09-11 03:48:34 +0300

    Make divs in container

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
+
 for (let i = 0; i < (16*16); i++) {
     const div = document.createElement('div');
-    div
+    container.appendChild(div);
 }
