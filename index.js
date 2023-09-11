302e593c5275b23ee3dbfed555b33bbd3e780a77
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-09-11 21:35:43 +0300

    Make change backgroundColor()
    that function change background color of div when mouse over on it

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const container = document.querySelector('.container');
 for (let column = 0; column < 16; column++) {
     for (let raw = 0; raw < 16; raw++) {
     const div = document.createElement('div');
+    div.className = 'cell';
     container.appendChild(div);
     }
 }
+
+function changeBackground(){
+    this.style.backgroundColor = 'black';
+}
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => cell.addEventListener("mouseover", changeBackground));
+
